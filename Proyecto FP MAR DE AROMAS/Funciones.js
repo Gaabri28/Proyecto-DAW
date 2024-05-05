7550b49e798455a9925cf47eed3066ba25273c11
Gaabri28 <146624112+Gaabri28@users.noreply.github.com>
2024-05-05 20:08:14 +0200

    Add files via upload

---
+++ Proyecto FP MAR DE AROMAS/Funciones.js
@@ -0,0 +1,29 @@
+function togglemobilemenu(element){
+    element.classList.toggle("change");
+    var mobileMenu = document.getElementById("mobilemenu");
+    mobileMenu.classList.toggle("open");
+    if (mobileMenu.style.display === "block") {
+        mobileMenu.style.display = "none"; 
+    } else {
+        mobileMenu.style.display = "block";
+    }
+}
+document.addEventListener("DOMContentLoaded", function() {
+  VanillaTilt.init(document.querySelectorAll(".Desplazable, .Item img"), {
+      max: 25,
+      speed: 400,
+  });
+});
+
+document.querySelector("#show-login").addEventListener("click",function(){
+    document.querySelector(".popup").classList.add("active");
+});
+document.querySelector(".red_btn").addEventListener("click",function(){
+    document.querySelector(".popup").classList.add("active");
+});
+document.querySelector(".popup .close-btn").addEventListener("click",function(){
+    document.querySelector(".popup").classList.remove("active");
+})
+
+
+
